351587080f14625fd752d5b08fa721e563dd0d41
Jonathan Renusch <jonathan@renusch.de>
2024-09-04 22:57:03 +0200

    chaanged some paths

--- slurm/job.sh
+++ slurm/job.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 config="$1"
-checkpoint="$2"
-mess="$3"
-data="$4"
-subset="$5"
-data_val="$6"
-out="$7"
-bs="$8"
-se="$9"
 
-python /raven/u/mvigl/raven_batch_example/test.py --config "$config" --checkpoint "$checkpoint" --mess "$mess" --data "$data" --subset "$subset" --data_val "$data_val" --out "$out" --bs "$bs" --start_epoch "$se"
+
+python /raven/u/jrenusch/raven_batch_hgpf/test.py --config "$config"
